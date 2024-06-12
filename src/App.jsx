9aad24ea8009478aeab3e0a80cd68c9647f71f32
Larysa Rud <larpavrud@gmail.com>
2024-06-11 20:54:30 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import {useState, useEffect} from "react";
+import "./App.css";
+
+import MovieDisplay from "./components/MovieDisplay";
+import Form from "./components/Form";
+
+export default function App() {
+  const apiKey = "98e3fb1f";
+
+  const [movie, setMovie] = useState(null);
+
+  const getMovie = async (searchTerm) => {
+    try {
+      const response = await fetch(
+        `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+      );
+      const data = await response.json();
+      setMovie(data);
+    } catch (e) {
+      console.error(e); // Handle any errors that may occur within our API request.
+    }
+  };
+
+// This will run on the first render but not on subsquent renders
+  useEffect(() => {
+    getMovie("Clueless");
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Movie Search</h1>
+      <Form moviesearch={getMovie} />
+      <MovieDisplay movie={movie} />
+    </div>
+  );
+}
